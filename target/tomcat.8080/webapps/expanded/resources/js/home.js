2aa0b5428756761edbf631be3b7a06925e9ffd58
Janel <janellogrono@gmail.com>
2021-01-01 03:09:57 +0800

    fixed config deploy web-app-runner

---
+++ target/tomcat.8080/webapps/expanded/resources/js/home.js
@@ -0,0 +1,34 @@
+function getInaanakByUserName(){
+
+	let fname = document.getElementById("inaanak-fname").value;
+		console.log('test: ' , fname);
+	const url = "http://localhost:8080/inaanak-system/getInaanak/" + fname;
+	
+	$('#exampleModal').modal({show: false});
+	
+	$.get(url,function(data,status){
+			console.log(`${data}`);
+			if(`${data}`){
+				$('#inaanak-found-modal').modal('show');
+				document.getElementById("inaanak-fname").disabled = true;
+				enableOtherFields();
+			}else{
+				$('#inaanak-notfound-modal').modal('show');
+			}
+		});
+}
+
+function enableOtherFields(){
+
+	document.getElementById("inaanak-mname").disabled = false;
+	document.getElementById("inaanak-lname").disabled = false;
+	
+	document.getElementById("ninong-fname").disabled = false;
+	document.getElementById("ninong-mname").disabled = false;
+	document.getElementById("ninong-lname").disabled = false;
+	
+	document.getElementById("ninong-favorites").disabled = false;
+	document.getElementById("ninong-kind").disabled = false;
+	document.getElementById("submit-btn").disabled = false;
+	
+}
