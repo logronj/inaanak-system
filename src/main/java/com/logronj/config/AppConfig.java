17543a60032db0dddaf03a563b9b6fd35fec8f95
Janel <janellogrono@gmail.com>
2021-01-01 04:31:50 +0800

    test

--- src/main/java/com/logronj/config/AppConfig.java
+++ src/main/java/com/logronj/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.logronj")
+@CrossOrigin
 public class AppConfig implements WebMvcConfigurer{
 
 	@Bean
--- src/main/java/com/logronj/controllers/InaanakController.java
+++ src/main/java/com/logronj/controllers/InaanakController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.logronj.repository.InaanakRepository;
 
 @RestController
-@CrossOrigin
 public class InaanakController {
 	
 	@Autowired
